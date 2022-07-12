a9078bfe86652c469b985db6b7113158b1ab18f2
codernike <108070610+codernike@users.noreply.github.com>
2022-07-12 23:20:07 +0530

    pokeblog1

--- src/compoents/footer.js
+++ src/compoents/footer.js
@@ -5,7 +5,7 @@ export default class footer extends Component {
     return (
      <>
     <div className='footer  w-100   page-footer center-on-small-only pt-0 mt-5 ' style={{marginTop:'100px'}}>
-     <footer className='p-5  mt-5 mb-0 bg-dark text-white text-center'><strong>Copyright © 2022 <a href='/' className=' text-white'>pikapkm.com</a> </strong></footer>
+     <footer className='p-5  mt-5 mb-0 bg-dark text-white text-center'><strong> <a href='/' className=' text-white'>Copyright © 2022 pikapkm.com</a> </strong></footer>
     </div>
      </>
     )
